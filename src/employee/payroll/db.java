d529895802d82f4dd792c62444c437f3a5813fa8
Progress Onyema <progress_onyema@yahoo.com>
2019-05-15 16:52:24 +0100

    Employee Paroll system in java: First Commit

---
+++ src/employee/payroll/db.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.payroll;
+
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author new
+ */
+public class db {
+    
+    
+    Connection conn = null;
+    public static Connection java_db() {
+        
+        try{
+            
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\new\\Documents\\NetBeansProjects\\Employee Payroll\\my_database.sqlite");
+            return conn;
+            
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+    
+}
